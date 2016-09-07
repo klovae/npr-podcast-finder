c04c7468511a4cc4f230449f14aa65ca55230ed6
Elyse Klova <elyse.klova@gmail.com>
2016-09-07 21:32:07 +0000

    started podcast scraper for category pages

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,24 +4,17 @@ require 'pry'
 
 class Scraper
 
-	def self.scrape_categories(index_url)
+	def self.start_scrape(index_url)
 		html = open(index_url)
 		index = Nokogiri::HTML(html)
-		category_names = self.get_category_list(index)
-		category_urls = self.get_category_urls(index)
-		count = 0
-		categories = []
-		until count == category_names.size - 1
-			category = {
-				:name => category_names[count],
-				:url => category_urls[count]
-			}
-			categories << category
-			count += 1
-		end
-		binding.pry
-		categories
 	end
+
+	def self.scrape_categories(index_url)
+		index = self.start_scrape(index_url)
+		self.compile_categories(index)
+	end
+
+	#helper methods for self.scrape_categories
 
 	def self.get_category_list(index)
 		category_list = []
@@ -42,5 +35,35 @@ class Scraper
 		end
 		category_links
 	end
+
+	def self.compile_categories(index)
+		category_names = self.get_category_list(index)
+		category_urls = self.get_category_urls(index)
+		count = 0
+		categories = []
+		until count == category_names.size - 1
+			category = {
+				:name => category_names[count],
+				:url => category_urls[count]
+			}
+			categories << category
+			count += 1
+		end
+		categories
+	end
+
+	# podcast scraping
+
+	def self.scrape_podcasts(category_url)
+		category = self.start_scrape(category_url)
+		self.get_podcast_data(category)
+	end
+
+	#helper methods for podcast scraping
+
+	def self.get_podcast_data(category)
+		podcasts = category.css('section#podcast-section-core article.podcast-active')
+	end
+
 
 end
