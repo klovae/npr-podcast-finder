93721bc3ae415584d2e23517284b18582a82da2c
Elyse Klova <elyse.klova@gmail.com>
2016-09-12 17:32:54 +0000

    changed Category and Podcast initializes methods to work with hashes

--- lib/category.rb
+++ lib/category.rb
@@ -1,22 +1,14 @@
 class Category
   extend Concerns::Findable
 
-  attr_accessor :name
-  attr_reader :url
+  attr_accessor :name, :url
+
 
   @@all = []
 
-  def initialize(name, url = nil)
-    @name = name
-    if url.nil? == false
-      self.url = url
-    end
-    @podcasts = []
-  end
-
-  def self.create(name, url = nil)
-    category = self.new(name, url = nil)
-    category.save
+  def initialize(category_hash)
+    category_hash.each {|key, value| self.send("#{key}=", value)}
+    self.save
   end
 
   def save
@@ -26,14 +18,4 @@ class Category
   def self.all
     @@all
   end
-
-  def url=(url)
-    @url = url
-  end
-
-  def self.create_from_hash(hash)
-    
-
-
-
 end
