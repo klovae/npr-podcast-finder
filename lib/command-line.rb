16689fadd012ae642e721c70a6c75c2646246f5d
Elyse Klova <elyse.klova@gmail.com>
2016-09-15 22:09:12 +0000

    wrote initial menu for CLI

--- lib/command-line.rb
+++ lib/command-line.rb
@@ -1,5 +1,19 @@
 class CommandLineInterface
 
+  def self.greeting
+    puts "Welcome to the Command Line Podcast Browser!"
+    puts "To get started, choose an option from the menu below:"
+    puts "(1) Browse podcast categories"
+    puts "(2) Browse podcasts by alphabet"
+    puts "(3) Search podcasts"
+    puts "(4) Discover podcasts (see a random selection)"
+    input = gets.strip
+  end
+
+  def self.start_import
+    DataImporter.import_categories('http://www.npr.org/podcasts')
+    DataImporter.import_podcast_data
+  end
 
 
 end
