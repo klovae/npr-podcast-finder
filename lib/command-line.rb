c335a7a3fc1c045b2b6685429ce8415d15bb6d0b
Elyse Klova <elyse.klova@gmail.com>
2016-09-16 16:47:17 +0000

    moved start sequence to its own method

--- lib/command-line.rb
+++ lib/command-line.rb
@@ -2,20 +2,23 @@ class CommandLineInterface
 
   attr_accessor :continue
 
-  def initialize(continue = "y")
+  def initialize(continue = "yes")
     @continue = continue
   end
 
   def call
-    puts "Setting up your command line podcast finder..."
-    puts "Estimated time: less than one minute."
-    start_import
-    puts "Setup complete."
-    puts "Welcome to the Command Line Podcast Finder!"
-    puts "You can use this command line gem to find and listen to interesting podcasts produced by NPR and affiliated stations."
-    start_menu
-    until gets.strip == "exit" do
-      #browser procedure here
+    startup_sequence
+    until continue == "exit" do
+      start_menu
+      input = gets.strip.upcase
+      case input
+      when input == "HELP"
+        help
+      when input == "MENU"
+        start_menu
+      when input == "EXIT"
+        continue = "exit"
+      end
     end
     "Thanks for using the Command Line Podcast Finder!"
   end
@@ -27,6 +30,23 @@ class CommandLineInterface
     puts "--Type 'help' if you need a quick reminder about the commands"
   end
 
+  def startup_sequence
+    puts "Setting up your command line podcast finder...".colorize(:light_red)
+    puts "Estimated time: less than one minute.".colorize(:light_red)
+    start_import
+    puts ".".colorize(:light_red)
+    sleep(1)
+    puts ".".colorize(:light_yellow)
+    sleep(1)
+    puts ".".colorize(:light_green)
+    sleep(1)
+    puts "Setup complete.".colorize(:light_green)
+    sleep(1)
+    puts "Welcome to the Command Line Podcast Finder!"
+    puts "You can use this command line gem to find and listen to interesting podcasts produced by NPR and affiliated stations."
+    sleep(1)
+  end
+
   def start_menu
     puts "Main Menu:"
     puts "To get started, choose an option below (1-4):"
@@ -34,6 +54,7 @@ class CommandLineInterface
     puts "(2) Browse podcasts by alphabet"
     puts "(3) Search podcasts"
     puts "(4) Discover podcasts (see a random selection)"
+    puts "Or, type 'help' to see a list of commands"
   end
 
   def start_import
