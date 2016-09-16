70e56de2f18b0093856c9e3520059dca601c6102
Elyse Klova <elyse.klova@gmail.com>
2016-09-16 14:17:23 +0000

    started building call method for cli

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem 'nokogiri'
+gem 'require_all'
+gem 'colorize', '~> 0.8.1'
 
 group :development do
 	gem 'pry'
@@ -11,6 +13,3 @@ end
 group :test do
 	gem 'rspec'
 end
-
-
-
--- lib/command-line.rb
+++ lib/command-line.rb
@@ -1,16 +1,42 @@
 class CommandLineInterface
 
-  def self.greeting
-    puts "Welcome to the Command Line Podcast Browser!"
-    puts "To get started, choose an option from the menu below:"
+  attr_accessor :continue
+
+  def initialize(continue = "y")
+    @continue = continue
+  end
+
+  def call
+    puts "Setting up your command line podcast finder..."
+    puts "Estimated time: less than one minute."
+    start_import
+    puts "Setup complete."
+    puts "Welcome to the Command Line Podcast Finder!"
+    puts "You can use this command line gem to find and listen to interesting podcasts produced by NPR and affiliated stations."
+    start_menu
+    until gets.strip == "exit" do
+      #browser procedure here
+    end
+    "Thanks for using the Command Line Podcast Finder!"
+  end
+
+  def help
+    puts "Help: Commands"
+    puts "--Type 'exit' at any time to quit the browser"
+    puts "--Type 'menu' at any time to go back to the main menu"
+    puts "--Type 'help' if you need a quick reminder about the commands"
+  end
+
+  def start_menu
+    puts "Main Menu:"
+    puts "To get started, choose an option below (1-4):"
     puts "(1) Browse podcast categories"
     puts "(2) Browse podcasts by alphabet"
     puts "(3) Search podcasts"
     puts "(4) Discover podcasts (see a random selection)"
-    input = gets.strip
   end
 
-  def self.start_import
+  def start_import
     DataImporter.import_categories('http://www.npr.org/podcasts')
     DataImporter.import_podcast_data
   end
